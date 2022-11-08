d0eb0125407482406ff2dbe94fa2a021558145b1
Ouzinem <ouzinem@hotmail.com>
2022-11-08 06:04:15 -0500

    create view  and view models for product list

---
+++ CNRBShop/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using CNRBShop.Services;
+using CNRBShop.Viewmodels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CNRBShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public ProductController(IProductRepository productRepository, ICategoryRepository categoryRepository)
+        {
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+            _categoryRepository = categoryRepository ?? throw new ArgumentNullException(nameof(categoryRepository));
+        }
+
+        public IActionResult List()
+        {
+            //ViewBag.CurrentCategory = "Dry fit man";
+            //return View(_productRepository.GetAllProducts);
+
+            ProductListViewModel productListViewModel = new ProductListViewModel
+                (_productRepository.GetAllProducts, "chandails");
+            return View(productListViewModel);
+        }
+    }
+}
---
+++ CNRBShop/Viewmodels/ProductListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CNRBShop.Models;
+
+namespace CNRBShop.Viewmodels
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public string? CurrentCategory { get; set; }
+
+        public ProductListViewModel(IEnumerable<Product> products, string? currentCategory)
+        {
+            Products = products;
+            CurrentCategory = currentCategory;
+        }
+    }
+}

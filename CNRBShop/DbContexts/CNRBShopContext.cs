4920ff1858401b9c5ebc2919fd3c568d88aa64a4
Ouzinem <ouzinem@hotmail.com>
2022-11-11 10:38:29 -0500

    add package to use sqlLite and configure  connection strings

---
+++ CNRBShop/DbContexts/CNRBShopContext.cs
@@ -0,0 +1,19 @@
+﻿using CNRBShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CNRBShop.DbContexts
+{
+    public class CNRBShopContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+         
+        public CNRBShopContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+
+    }
+}
--- CNRBShop/Program.cs
+++ CNRBShop/Program.cs
@@ -1,11 +1,19 @@
+using CNRBShop.DbContexts;
 using CNRBShop.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<ICategoryRepository,MockCategoryRepository>();
-builder.Services.AddScoped<IProductRepository,MockProductRepository>();
+builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+builder.Services.AddScoped<IProductRepository, MockProductRepository>();
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<CNRBShopContext>(
+    dbContextOptions =>
+    {
+        dbContextOptions.UseSqlite(builder.Configuration["ConnectionStrings:CNRBShopDBConnectionString"]);
+    });
 
 var app = builder.Build();
 

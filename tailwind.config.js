9d8311e43dbb952511de7eebc224d68af611b95c
Ouzinem <ouzinem@hotmail.com>
2023-09-28 16:41:34 -0400

    feat : style header

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
         './Views/**/*.cshtml'
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                brightRed: "hsl(12, 88%, 59%)",
+                brightRedLight: "hsl(12, 88%, 69%)",
+                brightRedSupLight: "hsl(12, 88%, 95%)",
+                darkBlue: "hsl(228, 39%, 23%)",
+                darkGrayishBlue: "hsl(227, 12%, 61%)",
+                veryDarkBlue: "hsl(233, 12%, 13%)",
+                veryPaleRed: "hsl(13, 100%, 96%)",
+                veryLightGray: "hsl(0, 0%, 98%)",
+                rebeccapurple: "rebeccapurple",
+                // green: "hsla(140, 79%, 17%, 1)",
+            },
+},
     },
     plugins: [],
 }
